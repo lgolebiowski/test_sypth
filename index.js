a69c970f1c3fa3eae6be243f92f8e41bc2405b3e
Lukasz Golebiowski <lukasz.golebiowski@gmail.com>
2019-08-11 22:06:23 +1000

    added get express config

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const fs = require('fs');
 const app = express();
 app.use(express.json());
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 const port = process.env.PORT || 5000;
 
 // console.log that your server is up and running
